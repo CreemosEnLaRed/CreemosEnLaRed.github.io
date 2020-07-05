b5facc8e2bbdf5fb125bb258dae859058148424f
Mariano Guerra <mariano@marianoguerra.org>
2020-07-05 16:44:31 +0200

    nueva version de bootstrap

--- codigo-web/js/monacoloader.js
+++ codigo-web/js/monacoloader.js
@@ -14,7 +14,7 @@ const defaultHTMLCode = `<!doctype html>
 <html>
   <head>
     <title>Titulo de Pagina</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
   </head>
   <body>
     <h1>Mi Pagina</h1>
